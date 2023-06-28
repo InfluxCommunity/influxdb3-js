374a92913ce9d7808a28c00b2f5f6728a9581103
Jakub Bednar <jakub.bednar@gmail.com>
2023-06-28 13:04:51 +0200

    fix: linter

--- packages/client/src/util/common.ts
+++ packages/client/src/util/common.ts
@@ -10,14 +10,14 @@ export const throwReturn = <T>(err: Error): Defined<T> => {
 export const isDefined = <T>(value: T): value is Defined<T> =>
   value !== undefined
 
-export const isArrayLike = <T>(value: any): value is ArrayLike<T> =>
+export const isArrayLike = <T>(value: any): value is ArrayLike<T> => /* eslint-disable explicit-module-boundary-types */
   value instanceof Array ||
   (value instanceof Object &&
     typeof value.length === 'number' &&
     (value.length === 0 ||
       Object.getOwnPropertyNames(value).some((x) => x === '0')))
 
-export const createInt32Uint8Array = (value: number) => {
+export const createInt32Uint8Array = (value: number): Uint8Array => {
   const bytes = new Uint8Array(4)
   bytes[0] = value >> (8 * 0)
   bytes[1] = value >> (8 * 1)
