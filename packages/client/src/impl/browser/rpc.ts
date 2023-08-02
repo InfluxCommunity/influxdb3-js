cb2ee3f7053f7d36b33668af666c9082b7b45814
Sciator <39964450+Sciator@users.noreply.github.com>
2023-08-02 03:14:43 +0200

    fix: don't manipulate url for web query

--- packages/client/src/impl/browser/rpc.ts
+++ packages/client/src/impl/browser/rpc.ts
@@ -1,9 +1,6 @@
 import {GrpcWebFetchTransport} from '@protobuf-ts/grpcweb-transport'
 import {CreateQueryTransport} from '../implSelector'
-import {replaceURLProtocolWithPort} from '../../util/fixUrl'
 
 export const createTransport: CreateQueryTransport = ({host}) => {
-  const {url} = replaceURLProtocolWithPort(host)
-
-  return new GrpcWebFetchTransport({baseUrl: url})
+  return new GrpcWebFetchTransport({baseUrl: host})
 }
