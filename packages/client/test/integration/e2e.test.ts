a536f772f90b90c943ab342e280ad828c49a5100
Sciator <39964450+Sciator@users.noreply.github.com>
2023-11-15 15:51:20 +0100

    test: big query, parallel test

--- packages/client/test/integration/e2e.test.ts
+++ packages/client/test/integration/e2e.test.ts
@@ -30,6 +30,21 @@ function getRandomInt(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
+const range: {
+  (max: number): number[]
+  (min: number, max: number, step?: number): number[]
+} = (n: number, n1?: number, step: number = 1): number[] => {
+  const length = n1 === undefined ? n : Math.max(Math.ceil((n1 - n) / step), 0)
+
+  return length >= 0
+    ? Array.from(Array(length).keys()).map(
+        n1 === undefined ? (x) => x : (x) => x * step + n
+      )
+    : []
+}
+
+const sleep = async (ms: number) => new Promise((r) => setTimeout(r, ms))
+
 describe('e2e test', () => {
   it('write and query data', async () => {
     const {database, token, url} = getEnvVariables()
@@ -106,5 +121,129 @@ describe('e2e test', () => {
 
     await client.close()
     await rejects(client.query(query, database, queryType).next())
+  })
+
+  it('parallel query', async () => {
+    const {database, token, url} = getEnvVariables()
+
+    const client = new InfluxDBClient({
+      host: url,
+      token,
+      writeOptions: {
+        precision: 'ms',
+      },
+    })
+
+    const testId = getRandomInt(0, 100000000)
+    const values = range(50).map(() => ({
+      avg: getRandomInt(110, 500),
+      max: getRandomInt(900, 1000),
+    }))
+
+    const time = Date.now()
+    const points = values.map(({avg, max}, i) =>
+      Point.measurement('stat')
+        .setTag('unit', 'temperature')
+        .setFloatField('avg', avg)
+        .setFloatField('max', max)
+        .setIntegerField('testId', testId)
+        .setTimestamp(time + i)
+    )
+    await client.write(points, database)
+
+    const query = `
+      SELECT *
+        FROM "stat"
+        WHERE
+        time >= now() - interval '10 minute'
+        AND
+        "testId" = ${testId}
+        ORDER BY time
+    `
+
+    const queryType = 'sql'
+
+    const paralelQueries = 8
+
+    const datas = await Promise.all(
+      range(paralelQueries)
+        .map(() => client.queryPoints(query, database, queryType))
+        .map(async (data) => {
+          const queryValues: typeof values = []
+          for await (const row of data) {
+            queryValues.push({
+              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+              avg: row.getFloatField('avg')!,
+              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+              max: row.getFloatField('max')!,
+            })
+            await sleep(10)
+          }
+          return queryValues
+        })
+    )
+
+    for (const data of datas) {
+      expect(data).to.deep.equal(values)
+    }
+
+    await client.close()
+  })
+
+  it.only('big query', async () => {
+    const {database, token, url} = getEnvVariables()
+
+    const client = new InfluxDBClient({
+      host: url,
+      token,
+      writeOptions: {
+        precision: 'ms',
+      },
+    })
+
+    const testId = getRandomInt(0, 100000000)
+    const values = range(50_000).map(() => ({
+      avg: getRandomInt(110, 500),
+      max: getRandomInt(900, 1000),
+    }))
+
+    const time = Date.now()
+    const points = values.map(({avg, max}, i) =>
+      Point.measurement('stat')
+        .setTag('unit', 'temperature')
+        .setFloatField('avg', avg)
+        .setFloatField('max', max)
+        .setIntegerField('testId', testId)
+        .setTimestamp(time + i)
+    )
+    await client.write(points, database)
+
+    const query = `
+      SELECT *
+        FROM "stat"
+        WHERE
+        time >= now() - interval '10 minute'
+        AND
+        "testId" = ${testId}
+        ORDER BY time
+    `
+
+    const queryType = 'sql'
+
+    const data = client.queryPoints(query, database, queryType)
+
+    const queryValues: typeof values = []
+    for await (const row of data) {
+      queryValues.push({
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        avg: row.getFloatField('avg')!,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        max: row.getFloatField('max')!,
+      })
+    }
+    expect(queryValues.length).to.equal(values.length)
+    expect(queryValues).to.deep.equal(values)
+
+    await client.close()
   })
 })
