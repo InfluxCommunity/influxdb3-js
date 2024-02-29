1dcdc36a6e4576ac9b0974156522981a09065eb3
karel rehor <karl.koerner@bonitoo.io>
2024-02-29 11:13:36 +0100

    test: adds e2e_test for queryPoints with query params

--- packages/client/test/integration/e2e.test.ts
+++ packages/client/test/integration/e2e.test.ts
@@ -266,60 +266,60 @@ describe('e2e test', () => {
     await client.close()
   }).timeout(40_000)
 
-  it('queries with parameters', async () => {
-    const samples = [
-      {
-        measurement: 'frame',
-        work: 'FortApache',
-        director: 'J_Ford',
-        reel: 7,
-        lumens: Math.PI,
-        integrity: 0.7,
-        sound: 4.5,
-        quality: 'Average',
-      },
-      {
-        measurement: 'frame',
-        work: 'Searchers',
-        director: 'J_Ford',
-        reel: 12,
-        lumens: 2 * Math.E,
-        integrity: 0.9,
-        sound: 0.07,
-        quality: 'Good',
-      },
-      {
-        measurement: 'frame',
-        work: 'BiggerThanLife',
-        director: 'N_Ray',
-        reel: 3,
-        lumens: 0.432,
-        integrity: 0.81,
-        sound: 2.5,
-        quality: 'Good',
-      },
-      {
-        measurement: 'frame',
-        work: 'Niagara',
-        director: 'H_Hathaway',
-        reel: 8,
-        lumens: 5.34,
-        integrity: 0.45,
-        sound: 3.03,
-        quality: 'Poor',
-      },
-      {
-        measurement: 'frame',
-        work: 'JohnnyGuitar',
-        director: 'N_Ray',
-        reel: 6,
-        lumens: 7.25,
-        integrity: 0.93,
-        sound: 1.07,
-        quality: 'Excellent',
-      },
-    ]
+  const samples = [
+    {
+      measurement: 'frame',
+      work: 'FortApache',
+      director: 'J_Ford',
+      reel: 7,
+      lumens: Math.PI,
+      integrity: 0.7,
+      sound: 4.5,
+      quality: 'Average',
+    },
+    {
+      measurement: 'frame',
+      work: 'Searchers',
+      director: 'J_Ford',
+      reel: 12,
+      lumens: 2 * Math.E,
+      integrity: 0.9,
+      sound: 0.07,
+      quality: 'Good',
+    },
+    {
+      measurement: 'frame',
+      work: 'BiggerThanLife',
+      director: 'N_Ray',
+      reel: 3,
+      lumens: 0.432,
+      integrity: 0.81,
+      sound: 2.5,
+      quality: 'Good',
+    },
+    {
+      measurement: 'frame',
+      work: 'Niagara',
+      director: 'H_Hathaway',
+      reel: 8,
+      lumens: 5.34,
+      integrity: 0.45,
+      sound: 3.03,
+      quality: 'Poor',
+    },
+    {
+      measurement: 'frame',
+      work: 'JohnnyGuitar',
+      director: 'N_Ray',
+      reel: 6,
+      lumens: 7.25,
+      integrity: 0.93,
+      sound: 1.07,
+      quality: 'Excellent',
+    },
+  ]
 
+  it('queries with parameters', async () => {
     const {database, token, url} = getEnvVariables()
 
     const time = Date.now()
@@ -367,7 +367,57 @@ describe('e2e test', () => {
 
     for await (const row of data) {
       expect(row['director']).to.equal('J_Ford')
-      expect(row['work'])
+    }
+  }).timeout(5_000)
+
+  it('queries to points with parameters', async () => {
+    const {database, token, url} = getEnvVariables()
+
+    const time = Date.now()
+
+    let lp = ''
+
+    for (let i = 0; i < samples.length; i++) {
+      lp +=
+        `${samples[i].measurement},work=${samples[i].work},` +
+        `director=${samples[i].director} reel=${samples[i].reel}i,` +
+        `lumens=${samples[i].lumens},integ=${samples[i].integrity},` +
+        `sound=${samples[i].sound},quality="${samples[i].quality}"` +
+        ` ${time - i * 60000}`
+      if (i < samples.length - 1) {
+        lp += '\n'
+      }
+    }
+
+    const client = new InfluxDBClient({
+      host: url,
+      token,
+      writeOptions: {
+        precision: 'ms',
+      },
+    })
+
+    await client.write(lp, database)
+
+    await sleep(3_000)
+
+    const query = `
+      SELECT *
+        FROM "${samples[0].measurement}"
+        WHERE
+        time >= now() - interval '10 minute'
+        AND
+        "director" = $director    
+    `
+    const points = client.queryPoints(
+      query,
+      database,
+      'sql',
+      new Map([['director', 'N_Ray']])
+    )
+
+    for await (const point of points) {
+      expect(point.getTag('director')).to.equal('N_Ray')
     }
   }).timeout(5_000)
 })
