418d31262585d2d9f42ea9faa25ef6ba880d5c0d
Jakub Bednar <jakub.bednar@gmail.com>
2024-06-03 14:42:54 +0200

    fix: script to update nightly version

--- .circleci/deploy-nightly-version.sh
+++ .circleci/deploy-nightly-version.sh
@@ -5,7 +5,7 @@ set -e
 SCRIPT_PATH="$( cd "$(dirname "$0")" || exit ; pwd -P )"
 
 # Update Version
-VERSION=$(< 'packages/client/src/impl/version.ts' sed 's/[^0-9.]*//g' | awk -F. '{$2+=1; OFS="."; print $1"."$2"."$3}')
+VERSION=$(head -n 1 'packages/client/src/impl/version.ts' | sed 's/[^0-9.]*//g' | awk -F. '{$2+=1; OFS="."; print $1"."$2"."$3}')
 sed -i -e "s/CLIENT_LIB_VERSION = '.*'/CLIENT_LIB_VERSION = '${VERSION}.nightly'/" packages/client/src/impl/version.ts
 yarn lerna version "$VERSION"-nightly."$CIRCLE_BUILD_NUM" --no-git-tag-version --yes
 git config user.name "CircleCI Builder"
