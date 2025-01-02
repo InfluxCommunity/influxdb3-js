1f402fed26415fc83501f5e1b33638d87b1579e6
vlastahajek <29980246+vlastahajek@users.noreply.github.com>
2025-01-02 15:18:46 +0100

    chore: refactored to benchmark style

--- packages/client/test/integration/client.ts
+++ packages/client/test/integration/client.ts
@@ -70,28 +70,63 @@ describe('benchmark', () => {
         };
         console.log("creating client");
         const client = await createFlightSqlClient(options);
-        console.log("executing query");
-        const start = Date.now();
-        const buffer = await client.query(queryLong);
-        const after = Date.now();
-        console.log("converting to table");
-        const batches = await RecordBatchReader.from(buffer);
-        var rows = [];
-        for await (const batch of batches) {
-            const row: Record<string, any> = {}
-            for (const batchRow of batch) {
-                for (const column of batch.schema.fields) {
-                    row[column.name] = batchRow[column.name]
+        //console.log("executing query");
+        const runQuery = async (query: string) : Promise<Record<string, any>[]> => {
+            //const start = Date.now();
+            const buffer = await client.query(query);
+            //const after = Date.now();
+            //console.log("converting to table");
+            const batches = await RecordBatchReader.from(buffer);
+            var rows = [];
+            for await (const batch of batches) {
+                const row: Record<string, any> = {}
+                for (const batchRow of batch) {
+                    for (const column of batch.schema.fields) {
+                        row[column.name] = batchRow[column.name]
+                    }
+                    rows.push(row);
                 }
-                rows.push(row);
             }
+            //const table = tableFromIPC(buffer);
+            //const end = Date.now();
+            //console.log(`query time: ${after - start}ms`);
+            //console.log(`table time: ${end - after}ms`);
+            return rows;
         }
-        //const table = tableFromIPC(buffer);
-        const end = Date.now();
-        console.log(`query time: ${after - start}ms`);
-        console.log(`table time: ${end - after}ms`);
+
+        const run = async (query: string, limit: number) => {
+            console.log(`running - ${query}`);
+
+            let totalTime = 0;
+            const arr = [];
+            for (let i = 0; i < 5; i++) {
+                const start = Date.now();
+                try {
+                    const result = await runQuery(query);
+                    //const result = await client.queryPoints(query);
+                    console.log(`reading start`)
+                    for await (const val of result) {
+                        arr.push(val);
+                    }
+                } catch (error) {
+                    console.error('Error executing query:', error);
+                }
+
+                //fs.writeFileSync('output.txt', JSON.stringify(arr));
+                const end = Date.now();
+                const elapsed = (end - start) / 1000;
+                console.log(`iter: ${i} \t ${elapsed.toFixed(6)}s`);
+                totalTime += elapsed;
+            }
+            console.log(`avg time: ${(totalTime / 5).toFixed(6)}s`);
+        }
+        var limit : number = 0;
+        for (let n = 1; n <= 2; n += 500) {
+            await run(queryLong, limit);
+            await new Promise((resolve) => setTimeout(resolve, 10000));
+        }
         //console.log(table);
-        console.log(`rows:  ${rows.length}`);
+        //console.log(`rows:  ${rows.length}`);
     }).timeout(300_000)
     it('query data Flight tablefromIpc', async () => {
         const options: ClientOptions = {
@@ -104,30 +139,66 @@ describe('benchmark', () => {
         };
         console.log("creating client");
         const client = await createFlightSqlClient(options);
-        console.log("executing query");
-        const start = Date.now();
-        const buffer = await client.query(queryLong);
-        const after = Date.now();
-        console.log("converting to table");
-        const table = tableFromIPC(buffer);
+        // console.log("executing query");
 
-        var rows = [];
-        for await (const batch of table.batches) {
-            const row: Record<string, any> = {}
-            for (const batchRow of batch) {
-                for (const column of batch.schema.fields) {
-                    row[column.name] = batchRow[column.name]
+        const runQuery = async (query: string) : Promise<Record<string, any>[]> => {
+            // const start = Date.now();
+            const buffer = await client.query(query);
+            //const after = Date.now();
+            //console.log("converting to table");
+            const table = tableFromIPC(buffer);
+
+            var rows = [];
+            for (const batch of table.batches) {
+                const row: Record<string, any> = {}
+                for (const batchRow of batch) {
+                    for (const column of batch.schema.fields) {
+                        row[column.name] = batchRow[column.name]
+                    }
+                    rows.push(row);
                 }
-                rows.push(row);
             }
+
+            // const end = Date.now();
+            // console.log(`query time: ${after - start}ms`);
+            // console.log(`table time: ${end - after}ms`);
+            // //console.log(table);
+            // console.log(`table schema: ${table.schema}`);
+            // console.log(`rows:  ${rows.length}`);
+            return rows;
         }
 
-        const end = Date.now();
-        console.log(`query time: ${after - start}ms`);
-        console.log(`table time: ${end - after}ms`);
-        //console.log(table);
-        console.log(`table schema: ${table.schema}`);
-        console.log(`rows:  ${rows.length}`);
+        const run = async (query: string, limit: number) => {
+            console.log(`running - ${query}`);
+
+            let totalTime = 0;
+            const arr = [];
+            for (let i = 0; i < 5; i++) {
+                const start = Date.now();
+                try {
+                    const result = await runQuery(query);
+                    //const result = await client.queryPoints(query);
+                    console.log(`reading start`)
+                    for await (const val of result) {
+                        arr.push(val);
+                    }
+                } catch (error) {
+                    console.error('Error executing query:', error);
+                }
+
+                //fs.writeFileSync('output.txt', JSON.stringify(arr));
+                const end = Date.now();
+                const elapsed = (end - start) / 1000;
+                console.log(`iter: ${i} \t ${elapsed.toFixed(6)}s`);
+                totalTime += elapsed;
+            }
+            console.log(`avg time: ${(totalTime / 5).toFixed(6)}s`);
+        }
+        var limit : number = 0;
+        for (let n = 1; n <= 2; n += 500) {
+            await run(queryLong, limit);
+            await new Promise((resolve) => setTimeout(resolve, 10000));
+        }
     }).timeout(300_000)
 
 })
