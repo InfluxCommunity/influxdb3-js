31899e29f48d315a31c0851fd0e4557ca6b41a58
karel rehor <karl.koerner@bonitoo.io>
2025-07-15 13:51:59 +0200

    chore: more lint fixes

--- packages/client/src/impl/browser/rpc.ts
+++ packages/client/src/impl/browser/rpc.ts
@@ -1,9 +1,11 @@
 import {GrpcWebFetchTransport} from '@protobuf-ts/grpcweb-transport'
 import {CreateQueryTransport} from '../implSelector'
 
-export const createTransport: CreateQueryTransport = (
-  {host, timeout, clientOptions}
-) => {
+export const createTransport: CreateQueryTransport = ({
+  host,
+  timeout,
+  clientOptions
+}) => {
 
   if (clientOptions) {
     console.warn(`Detected grpcClientOptions: such options are ignored in the GrpcWebFetchTransport:\n
