698b239e60d25b18422b31ac64f26f6f2c7d43c2
NguyenHoangSon96 <hoangsonnguyen4321@gmail.com>
2026-02-06 14:43:38 +0700

    chore: add parentheses around URL path fallback

--- packages/client/src/impl/node/NodeHttpTransport.ts
+++ packages/client/src/impl/node/NodeHttpTransport.ts
@@ -76,7 +76,7 @@ export class NodeHttpTransport implements Transport {
       timeout:
         nodeSupportedOptions.timeout ?? nodeSupportedOptions.writeTimeout,
     }
-    this._contextPath = proxyUrl ? _url : url.path ?? ''
+    this._contextPath = proxyUrl ? _url : (url.path ?? '')
     if (this._contextPath.endsWith('/')) {
       this._contextPath = this._contextPath.substring(
         0,
